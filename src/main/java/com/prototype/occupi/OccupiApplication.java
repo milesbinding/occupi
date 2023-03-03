62a009c036317091cb05a5965445950fac698c70
MILES-PC\Miles <mileslive@live.co.uk>
2023-03-03 17:53:00 +0000

    Initial commit / project setup

---
+++ src/main/java/com/prototype/occupi/OccupiApplication.java
@@ -0,0 +1,13 @@
+package com.prototype.occupi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccupiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OccupiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prototype/occupi/OccupiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.occupi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccupiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
