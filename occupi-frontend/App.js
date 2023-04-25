d57ba3fb3fc20f56f9d17855058c38a7f0f855e0
MILES-PC\Miles <mileslive@live.co.uk>
2023-04-25 11:18:36 +0100

    Improved styling, added map for accessing all devices from single endpoint, added error handling & interval for refreshing devices

--- occupi-frontend/App.js
+++ occupi-frontend/App.js
@@ -1,31 +1,27 @@
-import * as React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Rooms from './components/Rooms';
 import Settings from './components/Settings';
 
 export default class HomeNav extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
   render() {
     const Tab = createBottomTabNavigator();
 
     return (
-      <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-        headerShown: false, lazy: false, tabBaroptions: {  tabBarActiveTintColor: '#97deba', },
-      }}
-        initialRouteName="Rooms">
-        <Tab.Screen name="Rooms" component={Rooms} />
-        <Tab.Screen name="Settings" component={Settings} />
-      </Tab.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              headerShown: false,
+              lazy: false,
+              tabBaroptions: { tabBarActiveTintColor: '#97deba' },
+            }}
+            initialRouteName="Rooms"
+          >
+            <Tab.Screen name="Rooms" component={Rooms} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
     );
   }
 }
--- occupi-frontend/components/Rooms.js
+++ occupi-frontend/components/Rooms.js
@@ -4,35 +4,62 @@ import Images from '../images/Images';
 import moment from 'moment';
 
 class Rooms extends Component {
-  state = {
-    isLoading: true,
-    user: {},
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      devices: [],
+      error: null
+    };
+
+    // bind component methods to the component instance, this ensures the component refreshes
+    this.handleGetDevices = this.handleGetDevices.bind(this);
+  }
 
   componentDidMount() {
     this.handleGetDevices();
+    this.interval = setInterval(() => this.handleGetDevices(), 30000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   async handleGetDevices() {
     try {
-      const response = await fetch(`http://localhost:8080/devices/DC:68:EB:3B:7F:5A`, {
+      const response = await fetch(`http://localhost:8080/devices`, {
         method: 'GET',
         headers: {
           'Accept':'application/json'
         },
       });
-      const data = await response.json();
-      this.setState({
-        isLoading: false,
-        device: data,
-      });
+      if (response.status === 200) {
+        const data = await response.json();
+        this.setState({
+          isLoading: false,
+          devices: data,
+          error: null,
+        });
+      } else {
+        this.setState({
+          isLoading: false,
+          devices: null,
+          error: `Devices not found. ${error}`,
+        });
+      }
     } catch (error) {
       console.log(error);
+      this.setState({
+        isLoading: false,
+        devices: null,
+        error: `Something went wrong. ${error}`,
+      });
     }
   }
 
   render() {
-    const { isLoading, device } = this.state;
+    const { isLoading, devices } = this.state;
 
     if (isLoading) {
       return (
@@ -42,15 +69,31 @@ class Rooms extends Component {
       );
     }
 
+    if (this.state.error || !devices) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.errorText}>{this.state.error || 'No devices found.'}</Text>
+          <TouchableOpacity onPress={this.handleGetDevices} style={styles.refreshButton}>
+            <Text style={styles.refreshButtonText}>Try Again</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+
     return (
       <View style={styles.container}>
         <Image style={styles.image} source={Images.logo} />
-        <Text style={styles.name}>{` ${device.mac}`}</Text>
-        <Text style={styles.text}>{device.distance.substr(0, 4) + " meters away."}</Text>
-        <Text style={styles.text}>{"Last seen on " + moment(device.time_stamp).format("DD-MM-YYYY") + "."}</Text>
-        <Text style={styles.text}>{"Seen " + device.counter + " times."}</Text>
+        <Text style={styles.text}>Welcome to Room 1. The devices here refresh every 30 seconds.</Text>
+        {devices.map(device => (
+          <View style={styles.box} key={device.mac}>
+            <Text style={styles.name}>{` ${device.mac}`}</Text>
+            <Text style={styles.text}>{device.distance.substr(0, 4) + " meters away."}</Text>
+            <Text style={styles.text}>{"Last seen on " + moment(device.time_stamp).format("DD-MM-YYYY hh:mm A") + "."}</Text>
+            <Text style={styles.text}>{"Seen " + device.counter + " times."}</Text>
+          </View>
+        ))}
         <TouchableOpacity onPress={this.handleGetDevices} style={styles.refreshButton}>
-          <Text style={styles.refreshButtonText}>Refresh Device</Text>
+          <Text style={styles.refreshButtonText}>Refresh</Text>
         </TouchableOpacity>
       </View>
     );
@@ -65,12 +108,28 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   image: {
-    height: 200,
-    width: 200,
+    height: 250,
+    width: 250,
     marginBottom: 10,
   },
   loadingText: {
     fontSize: 20,
+  },
+  box: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 20,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
   },
   name: {
     fontSize: 24,
@@ -79,7 +138,9 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 16,
-    marginBottom: 30,
+    marginBottom: 15,
+    textAlign: 'center',
+    alignSelf: 'center',
   },
   refreshButton: {
     width: '50%',
@@ -92,6 +153,17 @@ const styles = StyleSheet.create({
   refreshButtonText: {
     color: 'black',
     fontSize: 18,
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 20,
+    padding: 10,
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+  placeholderText: {
+    fontSize: 20,
+    marginBottom: 30,
   },
 });
 
