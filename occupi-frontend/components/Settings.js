ac44fa929075065b3705e9518373ef54e2d68f6c
MILES-PC\Miles <mileslive@live.co.uk>
2023-05-02 13:16:25 +0100

    Add some more FAQ stuff and exit app handler for use on Android

--- occupi-frontend/components/Settings.js
+++ occupi-frontend/components/Settings.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  View, Image, StyleSheet, Text, TouchableOpacity, Modal,
+  View, Image, StyleSheet, Text, TouchableOpacity, Modal, BackHandler
 } from 'react-native';
 import Images from '../images/Images';
 
@@ -9,8 +9,8 @@ export default class Settings extends Component {
     showModal: false,
   };
 
-  handleFavRoom = () => {
-    // Code to handle changing the favorite room
+  handleExitApp = () => {
+      BackHandler.exitApp();
   };
 
   toggleModal = () => {
@@ -24,12 +24,13 @@ export default class Settings extends Component {
       <View style={styles.container}>
         <Image style={styles.image} source={Images.logo} />
         <Text style={styles.title}>Settings</Text>
-        <TouchableOpacity style={styles.logoutButton} onPress={this.handleFavRoom}>
-          <Text style={styles.buttonText}>Change Favorite Room</Text>
-        </TouchableOpacity>
         <TouchableOpacity style={styles.helpButton} onPress={this.toggleModal}>
-          <Text style={styles.helpButtonText}>Help and FAQ</Text>
+          <Text style={styles.helpButtonText}>Help & FAQ's</Text>
         </TouchableOpacity>
+        <TouchableOpacity style={styles.logoutButton} onPress={this.handleExitApp}>
+          <Text style={styles.buttonText}>Exit App</Text>
+        </TouchableOpacity>
+        
         <Modal
           visible={this.state.showModal}
           onRequestClose={this.toggleModal}
@@ -39,9 +40,46 @@ export default class Settings extends Component {
             <TouchableOpacity style={styles.closeButton} onPress={this.toggleModal}>
               <Text style={styles.closeButtonText}>Close</Text>
             </TouchableOpacity>
+            <Text style={styles.title}>
+              Help & FAQ's
+            </Text>
+
+            <Text style={styles.faqQuestion}>
+              What is this project/app?
+            </Text>
+
             <Text style={styles.faqText}>
-              This is the Help and FAQ section. Here you can find answers to frequently asked
-              questions about the app.
+              This project, Occupi, aims to measure the occupancy of a room using a Raspberry Pi, Bluetooth,
+              and Python. The collected data is extracted and stored in a Spring Boot + Maven API
+              written in Java, which can be accessed via this React Native JavaScript mobile front-end.
+            </Text>
+
+            <Text style={styles.faqQuestion}>
+              What does the app do?
+            </Text>
+
+            <Text style={styles.faqText}>
+              This app gathers data from surrounding Bluetooth devices and displays it in a clean, user friendly
+              way. This allows the user to make decisions on whether they want to go to the room, based on the 
+              number of people occupying the room at the current time.
+            </Text>
+
+            <Text style={styles.faqQuestion}>
+              What data is collected?
+            </Text>
+
+            <Text style={styles.faqText}>
+              Data gathered from the app is as followed: 
+
+              - Device name,
+              - MAC Address,
+              - Distance from Bluetooth beacon,
+              - Timestamp when device entered the room,
+              - Timestamp of device when last seen 
+              - Number of times the device has been seen.
+
+              All of this data is persisted in a database for as long as the device is in the room,
+              or 5 minutes after the device was last seen in the room.
             </Text>
           </View>
         </Modal>
@@ -106,10 +144,15 @@ const styles = StyleSheet.create({
   },
   closeButtonText: {
     fontSize: 18,
-    color: '#9b59b6',
+    color: '#97deba',
   },
   faqText: {
     fontSize: 18,
     marginBottom: 10,
+  },
+  faqQuestion: {
+    fontSize: 18,
+    marginBottom: 10,
+    fontWeight: 'bold',
   },
 });
